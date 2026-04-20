734d82ad23c6a6554a000185829d7f160188f4af
umesha-hb <umesha.kit@gmail.com>
2026-04-20 07:12:48 +0530

    first commit

---
+++ src/test/java/com/example/springjenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+    public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+    @Test
+    void contextLoads() {
+              logger.info("Test case Executing....");
+    }
+
+}
